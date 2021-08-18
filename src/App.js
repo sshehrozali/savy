8ecb3d94df626e04b7c44055f61c3c5fbf4e1cdd
Shehroz Ali <sshehroza13@gmail.com>
2021-08-18 16:41:54 +0500

    Rendered each amount component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import ReactDOM from "react-dom";
 import Savings from "./components/Savings";
 
 function App() {
+  // React States//
   // Store Current State value
   const [amount, setAmount] = React.useState("");
   // Store Deposited Amount value
   const [deposits, addDepositAmount] = React.useState([]);
 
+  // EVENT HANDLERS //
   // When Enter key is pressed
   function EnterKey(event) {
     if (event.key == "Enter") {
       event.preventDefault();
       addAmount();
-    };
-  };
+    }
+  }
   // When Button is clicked
   function ButtonPress(event) {
     event.preventDefault();
@@ -22,17 +24,21 @@ function App() {
   }
 
   // When User clicks the 'Deposit' Button
-  function addAmount(e) {
+  const addAmount = (e) => {
     // TODO
-    console.log(amount);
-  }
+    if (amount != "") {
+      addDepositAmount([...deposits, amount]); // Store each deposited amount value inside state
+      setAmount("");                           // Reset amount value to ""
+      console.log(`Amount Entered: ${amount}`);
+    }
+  };
 
   return (
     <div className="mainAppWindow">
       {/* Header */}
-      <nav class="navbar navbar-light bg-light">
+      <nav class="navbar navbar-light bg-light mb-5">
         <div class="container-fluid">
-          <a class="navbar-brand">My Wallet</a>
+          <a class="navbar-brand mb-2">My Wallet</a>
           <form class="d-flex">
             <input
               autoFocus
@@ -52,10 +58,17 @@ function App() {
         </div>
       </nav>
 
-      <div className="container">
-        <div class="card mt-5">
-          <div class="card-body fs-3">Rs.300</div>
-        </div>
+      {/* Amounts */}
+      <div className="showAmounts">
+        {deposits.map((element, index) => {
+          return (
+            <div className="container" key={ index }>
+              <div class="card mt-2">
+                <div class="card-body fs-3">Rs.{ element }</div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
