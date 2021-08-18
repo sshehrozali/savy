7927756b1861669226bdbde567cf228fdb1fb963
Shehroz Ali <sshehroza13@gmail.com>
2021-08-18 15:09:04 +0500

    Added React state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 function App() {
+
+  // Declared React useState()
+  const [state, setState] = React.useState({
+    amount: "",
+  });
+
+  // Function to update state.amount value
+  function updateVal(event) {
+    setState({
+      amount: event.target.value,
+    });
+  }
+
+  function add(e) {
+    e.preventDefault();
+    
+    // TODO
+  }
+
   return (
     <div className="mainAppWindow">
-      <h1 className="display-1">Hello World</h1>
+      <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand">My Wallet</a>
+          <form class="d-flex">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Enter Amount Here"
+              aria-label="Search"
+              onChange={updateVal}
+              id="amount-field"
+            />
+            <button class="btn btn-outline-success" onClick={add}>
+              Deposit
+            </button>
+          </form>
+        </div>
+      </nav>
     </div>
   );
 }
